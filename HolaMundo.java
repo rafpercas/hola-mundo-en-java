52e2b61224a188e1a60d73fd5b3656e9985aee3c
Rafael Perez Castaño <alum.rperezc.2021@iesalixar.org>
2021-10-05 19:56:42 +0200

    Saludo ampliado

--- HolaMundo.java
+++ HolaMundo.java
@@ -1,3 +1,8 @@
+/**
+ * Muestra por pantalla la frase "Hola Mundo!"
+ *
+ * @author Alan Brito
+ */ 
 public class HolaMundo {
  public static void main(String[] args) {
   System.out.println("Hola Mundo!");
